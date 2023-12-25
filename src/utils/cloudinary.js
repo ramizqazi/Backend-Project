e5c6480db3372008e72a5e219c8d362bf064a12e
ramizqazi <ramizqazi03@gmail.com>
2023-12-25 19:38:37 +0500

    add cloudinary setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, { resource_type: 'auto' });
+
+    // file has been uploaded successfully
+    console.log('file is uploaded successfully', response.url)
+
+    return response;
+
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+  }
+};
