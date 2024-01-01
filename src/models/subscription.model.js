fa35cfc609bc1b31ad13ca5164ba193fb3ce0cb2
ramizqazi <ramizqazi03@gmail.com>
2024-01-01 15:31:31 +0500

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const subscriptionSchema = new Schema({
+  subscriber: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  channel: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+}, { timestamps: true });
+
+export const Video = model('Subscription', subscriptionSchema);
