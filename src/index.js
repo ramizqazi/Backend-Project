88fb510cf7ede2f2aaf7fe10917cdb37e864bddd
ramizqazi <ramizqazi03@gmail.com>
2024-01-01 19:26:00 +0500

    removed unused code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
-import connectDb from './db/index.js';
+import mongoose from 'mongoose';
+
 import { app } from './App.js';
 
 dotenv.config({ path: './.env' })
 
-connectDb()
+mongoose.connect(`${process.env.MONGODB_URI}/test`)
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port: ${process.env.PORT}`)
